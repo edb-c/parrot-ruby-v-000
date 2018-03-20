d4ed09c2450cadd24a76bbbf6effa2fa5e646c55
Ethel C <cofresi.ethel@gmail.com>
2018-03-20 03:41:36 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot (input_phrase = "Squawk")
+def parrot (input_phrase = "Squawk!")
   puts input_phrase
   return input_phrase
 end
